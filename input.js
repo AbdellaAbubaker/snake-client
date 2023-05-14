00de51beec86dbaa146ff5ad37e27d91b1f1d6d4
AbdellaAbubaker <aabdella.abubaker@mgail.com>
2023-05-14 15:13:25 -0400

    complete snake project

--- input.js
+++ input.js
@@ -1,19 +1,38 @@
-
-
+let connection;
 
 const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      // Check for Ctrl + C input
+    if (key === "w") {
+      connection.write("Move: up");
+    } else if (key === "a") {
+      connection.write("Move: left");
+    } else if (key === "s") {
+      connection.write("Move: down");
+    } else if (key === "d") {
+      connection.write("Move: right");
+    } else if (key === "h") {
+      connection.write("Say: Hello!");
+    } else if (key === "g") {
+      connection.write("Say: Good game!");
+    } else if (key === "n") {
+      connection.write("Say: Nice to meet you!");
+    } else if (key === "\u0003") {
       process.exit();
     }
   };
   
-  const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    return stdin;
-  };
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+  
   
   module.exports = { setupInput };
